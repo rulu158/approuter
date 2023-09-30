904310ecd15efae5aef3764e1dc2540b9f5cf6e0
Raúl García <rulu158@gmail.com>
2023-09-30 12:31:44 +0200

    Add Cors configuration

--- src/main/java/dev/bracers/approuter/app/AppController.java
+++ src/main/java/dev/bracers/approuter/app/AppController.java
@@ -3,6 +3,7 @@ package dev.bracers.approuter.app;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.google.gson.JsonParser;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping(path="/app/{app}", produces="application/json")
 public class AppController {
